0c4429a47eb5272381a2108797a6af7622f8f7aa
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-08-17 19:11:17 +0200

    Fixed error on todosIguales(arreglo)

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -180,13 +180,13 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí
-  for (var i = 0; i < arreglo.length; i++) {
-    if (arreglo[i] === arreglo[i + 1]) {
-      return true;
-    } else {
+  for (var i = 0; i < arreglo.length - 1; i++) {
+    if (arreglo[i] !== arreglo[i + 1]) {
       return false;
     }
   }
+
+  return true;
 }
 
 function mesesDelAño(array) {
